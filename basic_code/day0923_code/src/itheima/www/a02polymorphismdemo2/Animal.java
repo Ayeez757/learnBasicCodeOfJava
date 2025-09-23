46ea3e95fdf9d19659ce7b67a8119e9b31d68179
Ayeez757 <3406608593@qq.com>
2025-09-23 20:06:23 +0800

    25.9.23晚，多态调用变量、方法的特点

---
+++ basic_code/day0923_code/src/itheima/www/a02polymorphismdemo2/Animal.java
@@ -0,0 +1,11 @@
+package itheima.www.a02polymorphismdemo2;
+
+public class Animal {
+    String name = "动物";
+
+    public void show(){
+        System.out.println("Animal---show");
+    }
+}
+
+
---
+++ basic_code/day0923_code/src/itheima/www/a02polymorphismdemo2/Dog.java
@@ -0,0 +1,10 @@
+package itheima.www.a02polymorphismdemo2;
+
+public class Dog extends Animal{
+    String name ="狗";
+
+    @Override
+    public void show(){
+        System.out.println("Dog---show");
+    }
+}
