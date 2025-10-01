261bd184ae220bff3b43d3ff1791fa1d20297e75
Ayeez757 <3406608593@qq.com>
2025-10-01 21:35:00 +0800

    25.10.01晚.html

--- basic_code/day0927_puzzleGame/src/com/itheima/puzzlegame/ui/GameJFrame.java
+++ basic_code/day0927_puzzleGame/src/com/itheima/puzzlegame/ui/GameJFrame.java
@@ -1,10 +1,13 @@
 package com.itheima.puzzlegame.ui;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
-//游戏界面的对象，继承JFrame，
-public class GameJFrame extends JFrame {
+//游戏界面的对象，继承JFrame，实现KeyListener键盘监听接口
+public class GameJFrame extends JFrame implements KeyListener {
 
 
     //创建二维数组
@@ -31,13 +34,21 @@ public class GameJFrame extends JFrame {
 
     }
 
+    //记录空白图片的索引坐标用于后续
+    int x;//内部数组索引
+    int y;//外部数组索引
+
     //打乱图片的方法
     private void initArr(){
 
         //创建一维数组
         int [] imgsArray1={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+
         //打乱一维数组
+        int count=1;
         Random r = new Random();
+        while(count%2!=0){
+            count--;
         for (int i = 0; i < imgsArray1.length; i++) {
             int index = r.nextInt(16);
             int temp = imgsArray1[i];
@@ -45,10 +56,36 @@ public class GameJFrame extends JFrame {
             imgsArray1[index] = temp;
 
         }
+        //判断华容道是否有解?????????????????????????????
+        int temp=0;
+
+        while(temp<15){
+
+            while(temp<15){
+                if(imgsArray1[temp]==imgsArray1[temp+1]-1){
+                    temp++;
+                    if(imgsArray1[temp]!=imgsArray1[temp+1]-1){
+                        temp++;
+                        count++;
+                        break;
+                    }
+                }
+                temp++;
+        }
+
+        }}
+
+
         //塞到二维数组里面去
         for(int i = 0 ; i < 4;i++){
             for (int j = 0; j < 4; j++) {
                 imgsArray2[i][j]=imgsArray1[i*4+j];
+                if(imgsArray2[i][j]==16){
+                    x = j;
+                    y = i;
+                    //测试用
+                    System.out.println(x+""+y);
+                }
             }
         }
 //        //检查是否打乱
@@ -63,6 +100,9 @@ public class GameJFrame extends JFrame {
     }
 
     private void initImage(){
+
+        //删除所有图片
+        this.getContentPane().removeAll();
 //        //图片名字的计数器
 //        int num = 1;
         //改为根据二维数组创建
@@ -74,18 +114,22 @@ public class GameJFrame extends JFrame {
                 //根据二维数组找图片
                 num = imgsArray2[i][j];
                 //创建imageIcon对象
-                ImageIcon icon = new ImageIcon("D:\\Develop\\Java\\Code\\Learn\\basic_code\\" +
-                        "day0927_puzzleGame\\imgs\\fufu\\_"+num+".jpg");
+                ImageIcon icon = new ImageIcon("day0927_puzzleGame\\imgs\\fufu\\_"+num+".jpg");
                 //创建一个jlabel对象
                 JLabel jLabel = new JLabel(icon);
                 //设置图片位于左上角
-                jLabel.setBounds(j*106,i*106,105,105);
+                jLabel.setBounds(j*105+84,i*105+143,105,105);
+                //给图片加个边框
+                jLabel.setBorder(new BevelBorder(0));
                 //把管理容器添加到界面
                 this.add(jLabel);
 //                //计数器加一
 //                num++;
             }
         }
+
+        //刷新页面
+        this.getContentPane().repaint();
     }
 
 
@@ -137,5 +181,86 @@ public class GameJFrame extends JFrame {
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         //取消内容自动居中
         this.setLayout(null);
+
+        //给界面加入键盘监听
+        this.addKeyListener(this);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //这个是松开触发的方法，在这里加入上下左右
+        //接收一下键盘的数据
+        int code = e.getKeyCode();
+        //判断上下左右
+        if(code==39){
+            if(x==3){
+                return;
+            }
+            System.out.println("右");
+            //现在对图片进行移动
+            /*
+            找到空白图片的索引坐标
+            与对应的图片索引坐标交换值
+            修改空白图片索引坐标
+            解决超出索引问题
+             */
+
+            //交换
+            int temp = imgsArray2[y][x];
+            imgsArray2[y][x]= imgsArray2[y][x+1];
+            imgsArray2[y][x+1]=temp;
+            x++;
+            initImage();
+
+        }
+        else if(code == 40){
+            if(y==3){
+                return;
+            }
+            System.out.println("下");
+            //交换
+            int temp =imgsArray2[y][x];
+            imgsArray2[y][x] = imgsArray2[y+1][x];
+            imgsArray2[y+1][x] = temp;
+            y++;
+            System.out.println(x+","+y);
+            initImage();
+        }
+        else if(code == 37){
+            if(x==0){
+                return;
+            }
+            System.out.println("左");
+            //交换
+            int temp = imgsArray2[y][x];
+            imgsArray2[y][x]=imgsArray2[y][x-1];
+            imgsArray2[y][x-1] = temp;
+            x--;
+            initImage();
+        }
+        else if(code == 38){
+            if(y==0){
+                return;
+            }
+            System.out.println("上");
+            //交换
+            int temp = imgsArray2[y][x];
+            imgsArray2[y][x]=imgsArray2[y-1][x];
+            imgsArray2[y-1][x]=temp;
+            initImage();
+            y--;
+        }
+        //实测如果移动图片会很反人类，移动空白会舒服很多
+
     }
 }
